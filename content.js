47d815291090d130860a5fc2eec1710b72d6470f
Richard <richl.laconte@gmail.com>
2020-01-27 19:56:10 -0500

    Storage Access Setup
    Can now successfully save items from the DOM and access them in popup.js/popup.html.

--- content.js
+++ content.js
@@ -4,7 +4,29 @@
 };*/
 
 let blocks = document.getElementById("previewEmail").contentWindow.document.getElementsByClassName("shsp-wireframe");
-console.log(blocks);
 let block = blocks[0];
-console.log(block);
-chrome.storage.sync.set({ block: block });
+let block1;
+let block2;
+let block3;
+let block4;
+
+// Cannot set more than 2 blocks to an array - error - "QUOTA_BYTS_PER_ITEM quota exceeded"
+// Instead need to try setting each block to its own variable.
+
+let blockString = block.innerHTML.toString();
+
+chrome.storage.sync.set({ block: block }, () => {
+    console.log('color is ' + block);
+})
+
+chrome.storage.sync.set({ blockString: blockString }, () => {
+    console.log('color is ' + blockString);
+})
+
+chrome.storage.sync.set({ blocks: blocks }, () => {
+    console.log('color is ' + blocks);
+})
+
+chrome.storage.sync.set({ array: array }, () => {
+    console.log('array =  ' + array);
+})
--- popup.js
+++ popup.js
@@ -1,24 +1,29 @@
 let changeColor = document.getElementById('changeColor');
+let scanPage = document.getElementById("scanPage");
 
 chrome.storage.sync.get('color', (data) => {
+    console.log(data);
     changeColor.style.backgroundColor = data.color;
     changeColor.setAttribute('value', data.color);
 })
 
 changeColor.onclick = (element) => {
+    console.log("test");
     let color = element.target.value;
     let test = () => {
         console.log("test has been run");
     }
-    /*chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.executeScript(
             tabs[0].id,
             //{ code: 'document.body.style.backgroundColor = "' + color + '";' });
-            { code:  });
-    });*/
+            { code: 'document.getElementById("previewEmail").contentWindow.document.getElementsByClassName("shsp-wireframe")[0].innerHTML = "test";' });
+    });
 };
+
+
+
 let code = document.getElementById('code');
 chrome.storage.sync.get(stored => {
-    console.log(stored.block.toSTring());
-    code.innerHTML = stored.block;
+    code.innerHTML = stored.blockString;
 });
