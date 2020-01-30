bf2482f5a5e9aa44bfc99fbcdb61b6c39a86741e
Richard <richl.laconte@gmail.com>
2020-01-29 19:57:19 -0500

    V1
    App now grabs the first 3 blocks from email and displays them in divs on the popup. Buttons have been added to navigate the blocks.

--- content.js
+++ content.js
@@ -4,41 +4,89 @@
 };*/
 
 let blocks = document.getElementById("previewEmail").contentWindow.document.getElementsByClassName("shsp-wireframe");
-let block = blocks[0];
-let block0;
-let block1;
-let block2;
-let block3;
-let block4;
+let block = [];
 
 // Cannot set more than 2 blocks to an array - error - "QUOTA_BYTS_PER_ITEM quota exceeded"
 // Instead need to try setting each block to its own variable.
 
+/*for (let i = 0; i < blocks.length; i++) {
+    if (block.getElementsByClassName("full content")[0].parentElement.innerHTML.toString().length >= 5000) {
+        let name = "block" + "" + i;
+        block.push(name);
+        let block1 = block.getElementsByClassName("full content")[0].parentElement.innerHTML.toString().slice(0, 5000);
+        chrome.storage.sync.set({ name: block1 }, () => {
+        })
+        name = "block" + "" + i + "" + "2";
+        block.push(name);
+        let block2 = block.getElementsByClassName("full content")[0].parentElement.innerHTML.toString().slice(5000, block.length);
+        chrome.storage.sync.set({ block: block2 }, () => {
+        })
+    } else {
+        let block1 = block.getElementsByClassName("full content")[0].parentElement.innerHTML.toString();
+        chrome.storage.sync.set({ block1String: block1 }, () => {
+        })
+    }
+}*/
 if (blocks[0]) {
-    block0 = blocks[0].innerHTML.toString();
+    if (blocks[0].getElementsByClassName("full").length > 0) {
+        console.log("blocks[0] length: " + blocks[0].getElementsByClassName("full")[0].parentElement.innerHTML.toString().length);
+        //block1 = blocks[1].innerHTML.toString();
+        if (blocks[0].getElementsByClassName("full")[0].parentElement.innerHTML.toString().length >= 5000) {
+            let block1string = blocks[1].getElementsByClassName("full")[0].parentElement.innerHTML.toString().slice(0, 5000);
+
+            let block12 = blocks[0].getElementsByClassName("full")[0].parentElement.innerHTML.toString().slice(5000, blocks[0].length);
+            chrome.storage.sync.set({ block0String: block1String }, () => {
+            })
+            chrome.storage.sync.set({ block02String: block12 }, () => {
+            })
+        } else {
+            let block1 = blocks[0].getElementsByClassName("full")[0].parentElement.innerHTML.toString();
+            chrome.storage.sync.set({ block0String: block1 }, () => {
+            })
+        }
+    }
 }
+
 if (blocks[1]) {
-    block1 = blocks[1].innerHTML.toString();
+    console.log("blocks[1] length: " + blocks[1].getElementsByClassName("full content")[0].parentElement.innerHTML.toString().length);
+    //block1 = blocks[1].innerHTML.toString();
+    if (blocks[1].getElementsByClassName("full content")[0].parentElement.innerHTML.toString().length >= 5000) {
+        let block1string = blocks[1].getElementsByClassName("full content")[0].parentElement.innerHTML.toString();
+        block1 = block1string.slice(0, 5000);
+
+        block12 = blocks[1].getElementsByClassName("full content")[0].parentElement.innerHTML.toString().slice(5000, blocks[1].length);
+        chrome.storage.sync.set({ block1String: block1 }, () => {
+        })
+        chrome.storage.sync.set({ block12String: block12 }, () => {
+        })
+    } else {
+        block1 = blocks[1].getElementsByClassName("full content")[0].parentElement.innerHTML.toString();
+        chrome.storage.sync.set({ block1String: block1 }, () => {
+        })
+    }
 }
 
-let blockString = block.innerHTML.toString();
+if (blocks[2]) {
+    if (blocks[2].getElementsByClassName("full").length > 0) {
+        console.log("blocks[2] length: " + blocks[2].getElementsByClassName("full")[0].parentElement.innerHTML.toString().length);
+        //block1 = blocks[1].innerHTML.toString();
+        if (blocks[2].getElementsByClassName("full")[0].parentElement.innerHTML.toString().length >= 5000) {
+            let block1string = blocks[2].getElementsByClassName("full")[0].parentElement.innerHTML.toString().slice(0, 5000);
 
-chrome.storage.sync.set({ block: block }, () => {
-    console.log('color is ' + block);
-})
+            let block12 = blocks[2].getElementsByClassName("full")[0].parentElement.innerHTML.toString().slice(5000, blocks[2].length);
+            chrome.storage.sync.set({ block2String: block1String }, () => {
+            })
+            chrome.storage.sync.set({ block22String: block12 }, () => {
+            })
+        } else {
+            let block1 = blocks[2].getElementsByClassName("full")[0].parentElement.innerHTML.toString();
+            chrome.storage.sync.set({ block2String: block1 }, () => {
+            })
+        }
+    }
+}
 
-chrome.storage.sync.set({ blockString: blockString }, () => {
-    console.log('color is ' + blockString);
-})
+/*
+if (blocks[2])
+*/
 
-chrome.storage.sync.set({ blocks: blocks }, () => {
-    console.log('color is ' + blocks);
-})
-
-chrome.storage.sync.set({ block0: block0 }, () => {
-    console.log('block0 ' + block0);
-})
-
-chrome.storage.sync.set({ block1: block1 }, () => {
-    console.log('block1 ' + block1);
-})
--- popup.js
+++ popup.js
@@ -1,13 +1,10 @@
-let changeColor = document.getElementById('changeColor');
-let scanPage = document.getElementById("scanPage");
+let refresh = document.getElementById('refresh');
+let copy = document.getElementById('copy');
+let paste = document.getElementById('paste');
+let previous = document.getElementById('previous');
+let next = document.getElementById('next');
 
-chrome.storage.sync.get('color', (data) => {
-    console.log(data);
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-})
-
-changeColor.onclick = (element) => {
+paste.onclick = (element) => {
     console.log("test");
     let color = element.target.value;
     let test = () => {
@@ -21,9 +18,62 @@ changeColor.onclick = (element) => {
     });
 };
 
+let currentBlock = 0;
 
+let code0 = document.getElementById('code0');
+let code1 = document.getElementById('code1');
+let code2 = document.getElementById('code2');
+let code3 = document.getElementById('code3');
 
-let code = document.getElementById('code');
+next.onclick = () => {
+    if (currentBlock < 3) {
+        currentBlock++;
+        renderCurrentBlock();
+    }
+}
+previous.onclick = () => {
+    if (currentBlock > 0) {
+        currentBlock--;
+        renderCurrentBlock();
+    }
+}
+
+let hideBlocks = () => {
+    code0.style.display = "none";
+    code1.style.display = "none";
+    code2.style.display = "none";
+    code3.style.display = "none";
+}
+
+let renderCurrentBlock = () => {
+    hideBlocks();
+    if (currentBlock === 0) {
+        code0.style.display = "block";
+    } else if (currentBlock === 1) {
+        code1.style.display = "block";
+    } else if (currentBlock === 2) {
+        code2.style.display = "block";
+    } else if (currentBlock === 3) {
+        code3.style.display = "block";
+    }
+}
+
+renderCurrentBlock();
+
 chrome.storage.sync.get(stored => {
-    code.innerHTML = stored.blockString;
+    code0.innerHTML = stored.block0String;
+    if (stored.block02String) {
+        code0.innerHTML += stored.block02String;
+    }
+
+    code1.innerHTML = stored.block1String;
+    if (stored.block12String) {
+        code1.innerHTML += stored.block12String;
+    }
+    code1.innerHTML += stored.block12String;
+
+    code2.innerHTML = stored.block2String;
+    if (stored.block22String) {
+        code2.innerHTML += stored.block22String;
+    }
 });
