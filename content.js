46c29fae9ff71ffe3b2bf71e6143e0666ce6dd4d
Richard LaConte <38797549+pakjones@users.noreply.github.com>
2020-02-08 17:55:28 -0500

    Starting set template
    Having trouble accessing window.api, may need to write a script to page to call.

--- content.js
+++ content.js
@@ -1,3 +1,37 @@
+const parse = (html) => {
+    let base = html;
+    let test = base.search('plzwork1');
+    let found = base.substr(test, 80);
+    let goodStuff = found.split('"')[2];
+    templateID = goodStuff.split('/')[3];
+    return goodStuff;
+}
+async function get(url) {
+    const response = await fetch(url, {});
+    const text = await response.text();
+
+    return text;
+}
+
+let templateHTML;
+let addedBlocksHTML;
+let templateID;
+let log = () => {
+    console.log(templateHTML);
+}
+window.setTimeout(log, 3000);
+get('/email/').then(text => {
+    get(parse(text))
+        .then(res => {
+            let start = res.search('"emailHTML":');
+            let end = res.search('"emailText":');
+            let found = res.substr((start + 13), (end - start - 15));
+            templateHTML = found;
+        })
+})
+
+
+
 const setStrings = (index, div) => {
     let string = div.outerHTML.toString();
 
@@ -48,11 +82,27 @@ const checkForControls = (block, number) => {
             emailControls.getElementsByClassName("email-block-controls-clone")[0].outerHTML = newButton;
             emailControls.getElementsByClassName("email-block-controls-recycle")[0].addEventListener("click", () => {
                 console.log("clicked block " + number);
+                let html = document.getElementById("previewEmail").contentWindow.document.querySelectorAll("[sh-layout]")[number].outerHTML;
+                addedBlocksHTML += html;
+                setEmail();
             })
         }
     }
 }
 
+const setEmail = () => {
+    window.api.setEmail(templateID, {
+        "unsubscribeCategoryID": 0,
+        "subject": "tracked?",
+        "dynamicSubject": "",
+        "dynamicSubjectAudienceID": "",
+        "fromName": "Market Marketing",
+        "fromEmail": "richl.laconte@test.com",
+        "title": "tracked?",
+        "description": "",
+        "emailHTML": templateHTML + "" + addedBlocksHTML
+    }, "test", "test", "test", (e) => { console.log(e) }, null);
+}
 
 window.setTimeout(main, 1000);
 /*
